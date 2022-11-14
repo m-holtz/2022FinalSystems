0c6a0c9704fa346b03881ee10706a5f4c5126297
m-holtz <mholtz@newschool.edu>
2022-11-14 17:03:24 -0500

    Start Over Button

---
+++ Final Project/Assets/Project/Scripts/Reset.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+    public class Reset : MonoBehaviour
+    {
+        public void ResetGame()
+        {
+            SceneManager.LoadScene("CookieMaker");
+
+        }
+
+    }
+
